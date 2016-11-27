f161196806c2ccae3e52a7ef858354d5c4924757
jakeiwen <iwenx006@umn.edu>
2016-11-27 15:58:44 -0600

    sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE salaries (
+  id integer,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  title VARCHAR(50),
+  salary INTEGER
+);
+
+INSERT INTO salaries (id, first_name, last_name, title, salary)
+VALUES
+(3, 'jake', 'iwen', 'bum', 1),
+(7, 'john', 'johnathan', 'professional' , 100000);
